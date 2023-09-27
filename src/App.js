58cdc267655b6d845102ff3d3138e0ee31e19bee
Falgun Patel <falgunmpatel9123@gmail.com>
2023-09-28 02:31:49 +0530

    Deploy

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import Leaderboard from "./components/Leaderboard";
 import QuizStats from "./components/QuizStats";
 import Home from "./components/Home";
 import Home2 from "./components/Home2";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createHashRouter,
+  HashRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 import QuizScience from "./components/Quiz/QuizScience";
 import QuizHistory from "./components/Quiz/QuizHistory";
 import QuizSports from "./components/Quiz/QuizSports";
 
 export default function App() {
   return (
-    <HashRouter>
+    <HashRouter basename="https://falgunmpatel.github.io/Assignment_DOTIX/#/app">
       <Routes>
         <Route path="/" Component={Home} />
         <Route path="/home2" Component={Home2} />
