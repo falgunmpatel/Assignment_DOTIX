2c1bebb746886c6cc3f3ffbe4cf80ea5fd30c4ee
Falgun Patel <falgunmpatel9123@gmail.com>
2023-09-27 22:52:55 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from "react";
-import Quiz from "./components/Quiz";
 import "./index.css";
 import Leaderboard from "./components/Leaderboard";
 import QuizStats from "./components/QuizStats";
 import Home from "./components/Home";
 import Home2 from "./components/Home2";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { render } from "react-dom";
 import QuizScience from "./components/Quiz/QuizScience";
 import QuizHistory from "./components/Quiz/QuizHistory";
 import QuizSports from "./components/Quiz/QuizSports";
 
-const [quiz, setQuiz] = "";
-const handleRedirect = (response) => {
-  setQuiz(response);
-};
-
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home onRedirect={handleRedirect} />,
+    element: <Home />,
   },
   {
     path: "/home2",
