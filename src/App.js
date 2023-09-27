900e168e01ac11144f4be0e150cbf0d32c076f30
Falgun Patel <falgunmpatel9123@gmail.com>
2023-09-28 02:13:14 +0530

    Deploy

--- src/App.js
+++ src/App.js
@@ -4,21 +4,14 @@ import Leaderboard from "./components/Leaderboard";
 import QuizStats from "./components/QuizStats";
 import Home from "./components/Home";
 import Home2 from "./components/Home2";
-import {
-  createBrowserRouter,
-  createHashRouter,
-  HashRouter,
-  Route,
-  RouterProvider,
-  Routes,
-} from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import QuizScience from "./components/Quiz/QuizScience";
 import QuizHistory from "./components/Quiz/QuizHistory";
 import QuizSports from "./components/Quiz/QuizSports";
 
 export default function App() {
   return (
-    <HashRouter>
+    <HashRouter basename="/app">
       <Routes>
         <Route path="/" Component={Home} />
         <Route path="/home2" Component={Home2} />
