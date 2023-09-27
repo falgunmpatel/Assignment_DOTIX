4c76ea0a065a4254bfb10585c21c0619e144143a
Falgun Patel <falgunmpatel9123@gmail.com>
2023-09-28 02:23:01 +0530

    Deploy

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import QuizSports from "./components/Quiz/QuizSports";
 
 export default function App() {
   return (
-    <HashRouter basename="https://falgunmpatel.github.io/Assignment_DOTIX/#/app">
+    <HashRouter basename="/">
       <Routes>
         <Route path="/" Component={Home} />
         <Route path="/home2" Component={Home2} />
