cd0f99932248d724bc65de618731386855a7eec6
Falgun Patel <falgunmpatel9123@gmail.com>
2023-09-29 00:51:02 +0530

    Router Updated

--- src/App.js
+++ src/App.js
@@ -4,30 +4,42 @@ import Leaderboard from "./components/Leaderboard";
 import QuizStats from "./components/QuizStats";
 import Home from "./components/Home";
 import Home2 from "./components/Home2";
-import {
-  createBrowserRouter,
-  createHashRouter,
-  HashRouter,
-  Route,
-  RouterProvider,
-  Routes,
-} from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import QuizScience from "./components/Quiz/QuizScience";
 import QuizHistory from "./components/Quiz/QuizHistory";
 import QuizSports from "./components/Quiz/QuizSports";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/home2",
+    element: <Home2 />,
+  },
+  {
+    path: "/quiz/science",
+    element: <QuizScience />,
+  },
+  {
+    path: "/quiz/history",
+    element: <QuizHistory />,
+  },
+  {
+    path: "/quiz/sports",
+    element: <QuizSports />,
+  },
+  {
+    path: "/quizstats",
+    element: <QuizStats />,
+  },
+  {
+    path: "/leaderboard",
+    element: <Leaderboard />,
+  },
+]);
+
 export default function App() {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" Component={Home} />
-        <Route path="/home2" Component={Home2} />
-        <Route path="/quiz/science" Component={QuizScience} />
-        <Route path="/quiz/history" Component={QuizHistory} />
-        <Route path="/quiz/sports" Component={QuizSports} />
-        <Route path="/quizstats" Component={QuizStats} />
-        <Route path="/leaderboard" Component={Leaderboard} />
-      </Routes>
-    </HashRouter>
-  );
+  return <RouterProvider router={router} />;
 }
